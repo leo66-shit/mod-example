a3dfb1629408b87b8f596aec0ed829b0a81042b0
leo66-shit <13367257650@163.com>
2026-06-13 13:57:44 +0800

    1.0.0

---
+++ src/client/java/leo66_/example/client/Hello_minecraftClient.java
@@ -0,0 +1,10 @@
+package leo66_.example.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Hello_minecraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/leo66_/example/client/Hello_minecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package leo66_.example.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Hello_minecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
